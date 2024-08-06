814bad4de15dc8efb78eef7c620af07cbd69a5d2
juwon <juwon0481@gmail.com>
2024-08-06 09:13:45 +0900

    레이아웃 수정

--- main.js
+++ main.js
@@ -2,10 +2,15 @@ import "./src/styles/main.scss";
 import "./src/lib/jujeob.js";
 import "./src/lib/randomDice.js";
 
-setTimeout(function () {
+function documentLoad() {
   const pageElement = document.querySelector("main");
   const pageHeader = document.querySelector("header");
-  pageHeader.style.display = "flex";
+  pageHeader.style.display = "none";
+  pageElement.style.display = "none";
 
-  pageElement.style.display = "block";
-}, 100);
+  setTimeout(function () {
+    pageHeader.style.display = "flex";
+    pageElement.style.display = "block";
+  }, 100);
+}
+documentLoad();
